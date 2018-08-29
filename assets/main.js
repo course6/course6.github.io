b7db49a54615f4b118d65f7d87cd6523f6fcd736
projectormato <k.keita811.anuy@gmail.com>
2018-08-30 02:12:38 +0900

    add validate selectday.

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,26 @@
 $(function () {
-var $body = $('body');
+  var $body = $('body');
 
-//開閉用ボタンをクリックでクラスの切替え
-$('#menu__btn').on('click', function (e) {
-  if ($body.hasClass('open')) {
+  //開閉用ボタンをクリックでクラスの切替え
+  $('#menu__btn').on('click', function (e) {
+    if ($body.hasClass('open')) {
+      $body.removeClass('open');
+    }else{
+      $body.addClass('open');
+    }
+  });
+
+  // メニュー名以外の部分をクリックで閉じる
+  $('#menu__nav').on('click', function () {
     $body.removeClass('open');
-  }else{
-    $body.addClass('open');
-  }
-});
+  });
 
-// メニュー名以外の部分をクリックで閉じる
-$('#menu__nav').on('click', function () {
-  $body.removeClass('open');
-});
+  $('#select-day')
+    .focusout(function(e) {
+      let SelectDay = new Date($(this).val()).getDay();
+      console.log(SelectDay);
+      if (!SelectDay || SelectDay == 6 || SelectDay == 0) {
+        $(this).after('<span class="text-red">平日を入力してください</span>')
+      }
+    });
 });
