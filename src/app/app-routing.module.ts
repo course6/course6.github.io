bdb5c67018fe263388dbd5bcd719dc5482e03b04
projectormato <k.keita811.anuy@gmail.com>
2021-08-21 17:01:07 +0900

    feat: ページ遷移時ページ先頭に飛ぶようにした

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,7 +42,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' }),
     TopPageModule,
     AboutPageModule,
     FaqPageModule,
