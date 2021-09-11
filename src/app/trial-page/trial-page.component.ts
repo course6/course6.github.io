b58e38c2103b60c444e35e54f2d819ba3c44eeac
projectormato <k.keita811.anuy@gmail.com>
2021-09-11 21:09:32 +0900

    fix: 無料体験申し込みページで土日を選択したときメッセージが出るように修正

--- src/app/trial-page/trial-page.component.ts
+++ src/app/trial-page/trial-page.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-trial-page',
   templateUrl: './trial-page.component.html',
-  styleUrls: ['./trial-page.component.scss']
+  styleUrls: ['./trial-page.component.scss'],
 })
 export class TrialPageComponent implements OnInit {
+  isSelectWeekday: boolean = true;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  changeDate(event: any) {
+    const selectedDay = event.target.valueAsDate.getDay();
+    this.isSelectWeekday = !(!selectedDay || selectedDay == 6 || selectedDay == 0);
   }
-
 }
+
