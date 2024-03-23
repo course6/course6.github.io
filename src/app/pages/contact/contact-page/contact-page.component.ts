ee8581d4906c187f3cbb0ff765469860ddb50ff8
projectormato <k.keita811.anuy@gmail.com>
2024-03-23 19:34:52 +0900

    feat: お問い合わせ 調整

--- src/app/pages/contact/contact-page/contact-page.component.ts
+++ src/app/pages/contact/contact-page/contact-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
+declare var gtag: any;
+
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
@@ -13,5 +15,12 @@ export class ContactPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.titleService.setTitle(`${this.title} | マイネット進学`);
+  }
+
+  gtagConversion() {
+    gtag('config', 'AW-10800092951');
+    gtag('event', 'conversion', {
+      send_to: 'AW-10800092951/G3biCOTGuIADEJeu8Z0o',
+    });
   }
 }
